f76e282e8729c5b640b93f942c3b1333f09e60c0
Jacob Libby <jacobmlibby@gmail.com>
2024-02-19 10:19:42 -0700

    created better output formatting and only included useful output variables

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ import urllib3
 def main():
 
     api = srcomapi.SpeedrunCom(); api.debug = 0
-
-    game = api.search(dt.Game, {"name": "borderlands 2"})[0]
-    table = Texttable()
-    table.set_cols_dtype(['i','t'])
+    gameName = "borderlands 2"
+    game = api.search(dt.Game, {"name": gameName})[0]
+    tableCategories = Texttable()
+    tableCategories.set_cols_dtype(['i','t'])
     categories = []
-    table.add_rows([["Index","Current Categories"]])
+    tableCategories.add_rows([["Index","Current Categories"]])
     for eachCategory in range(len(game.categories)):
-        table.add_row([eachCategory,str(game.categories[eachCategory])[11:-2]])
+        tableCategories.add_row([eachCategory,str(game.categories[eachCategory])[11:-2]])
     # t.add_rows([["Current Categories"], (print(', ["' + category + ']"') for category in categories)])
 
     # t.add_rows(eachCategory))
     print("Please select a speedrunning category with input 0-" + str(len(game.categories)) + " and hit 'enter'")
-    print(table.draw())
+    print(tableCategories.draw())
     # print(game)
     # print(game.categories)
     # print(game.categories[0].records[0].runs)
@@ -36,17 +36,68 @@ def main():
         else:
             break
     print("You have selected: " + str(game.categories[chosenCat])[11:-2])
+    print("Printing the data for the fastest " + str(game.categories[chosenCat])[11:-2] + " run:")
 
-    print(game.categories[chosenCat].records[0].runs)
-    print(len(game.categories[chosenCat].records[0].runs))
+    # print(game.categories[chosenCat].records[0].runs)
+    # print(len(game.categories[chosenCat].records[0].runs))
 
-    print(game.categories[chosenCat].records[0].runs[0]["run"].embeds)
+    # print(game.categories[chosenCat].records[0].runs[0]["run"].embeds)
     
-
-    data = ['category', 'comment', 'data', 'date', 'embeds', 'endpoint', 'game', 'id', 'level', 'players', 'players', 'splits', 'status', 'submitted', 'system', 'times', 'values', 'videos', 'weblink']
+    tableRuns = Texttable()
+    tableRuns.set_cols_dtype(['t','t'])
+    tableRuns.add_rows([["Variable","Values"]])
+    tableRuns.add_row(['game',gameName])
+    tableRuns.add_row(["category",str(game.categories[chosenCat])[11:-2]])
+    # data = ['category', 'comment', 'data', 'date', 'embeds', 'endpoint', 'game', 'id', 'level', 'players', 'players', 'splits', 'status', 'submitted', 'system', 'times', 'values', 'videos', 'weblink']
+    data = ['comment', 'date', 'endpoint', 'id', 'level', 'players', 'splits', 'status', 'submitted', 'system', 'times', 'values', 'videos', 'weblink']
     for eachData in data:
-        print("**" + eachData + ':     ' + str(getattr(game.categories[chosenCat].records[0].runs[0]["run"],eachData)))
-    
+        # print("**" + eachData + ':     ' + str(getattr(game.categories[chosenCat].records[0].runs[0]["run"],eachData)))
+        if eachData == "players":
+            playerStr = ""
+            for eachPlayer in range(len(getattr(game.categories[chosenCat].records[0].runs[0]["run"],eachData))):
+                if eachPlayer != 0:
+                    playerStr += ", "
+                playerStr += str(getattr(game.categories[chosenCat].records[0].runs[0]["run"],eachData)[eachPlayer])[7:-2]
+            tableRuns.add_row([eachData,playerStr])
+        elif eachData == "status":
+            if getattr(game.categories[chosenCat].records[0].runs[0]["run"],eachData)['status'] != 'verified':
+                tableRuns.add_row([eachData,'status: ' + str(getattr(game.categories[chosenCat].records[0].runs[0]["run"],eachData)['status'])])
+            else:
+                statusStr = ""
+                for k, v in getattr(game.categories[chosenCat].records[0].runs[0]["run"],eachData).items():
+                    if str(k) == 'verify-date':
+                        statusStr += str(k) + ": " + str(v).replace("T", " ").replace("Z", "") + '\n'
+                    else:
+                        statusStr += str(k) + ": " + str(v) + '\n'
+            statusStr = statusStr[:-1]
+            tableRuns.add_row([eachData,statusStr])
+        elif eachData == "submitted":
+            tableRuns.add_row([eachData,'status: ' + str(getattr(game.categories[chosenCat].records[0].runs[0]["run"],eachData)).replace("T", " ").replace("Z", "")])
+        elif eachData == "system":
+            systemStr = ""
+            for k, v in getattr(game.categories[chosenCat].records[0].runs[0]["run"],eachData).items():
+                systemStr += str(k) + ": " + str(v) + '\n'
+            systemStr = systemStr[:-1]
+            tableRuns.add_row([eachData,systemStr])
+        elif eachData == "times":
+            timesStr = ""
+            for k, v in getattr(game.categories[chosenCat].records[0].runs[0]["run"],eachData).items():
+                if str(k)[-2:] != "_t":
+                    if str(v)[0:2] == "PT":
+                        timesStr += str(k) + ": " + str(v)[2:].replace("H","H ").replace("M","M ") + '\n'
+                    else:
+                        timesStr += str(k) + ": " + str(v) + '\n'
+            timesStr = timesStr[:-1]
+            tableRuns.add_row([eachData,timesStr])
+        elif eachData == "videos":
+            videosStr = ""
+            for links, uri in getattr(game.categories[chosenCat].records[0].runs[0]["run"],eachData).items():
+                videosStr += str(uri)[10:-3] + ', '
+            videosStr = videosStr[:-2]
+            tableRuns.add_row([eachData,videosStr])
+        else:
+            tableRuns.add_row([eachData,str(getattr(game.categories[chosenCat].records[0].runs[0]["run"],eachData))])
+    print(tableRuns.draw())
     # sms_runs = {}
     # for category in game.categories:
     #     if not category.name in sms_runs:
